54a07a2492a0b6c64a496e13e89d566e0949613c
25662016-ai <25662016@kthcm.edu.vn>
2026-09-12 01:51:22 +0000

    đồng bộ 12/9

--- hello_dart/bin/hello_dart.dart
+++ hello_dart/bin/hello_dart.dart
@@ -1,5 +1,10 @@
 import 'package:hello_dart/hello_dart.dart' as hello_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${hello_dart.calculate()}!');
+void main() {
+  final ten = 'sinh vien';
+  print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
+  FOR (var i = 0; i < 5; i++) {
+    print('Dem:$i');
+  }
 }
+
